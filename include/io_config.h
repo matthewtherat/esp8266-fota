78ebf0c765279476578047c2332c38cfe950cc73
pylover <vahid.mardani@gmail.com>
2018-12-25 00:43:42 +0330

    io_config file

---
+++ include/io_config.h
@@ -0,0 +1,15 @@
+#ifndef _IO_CONFIG_H__
+#define _IO_CONFIG_H__
+
+#define EASYQ_RECV_BUFFER_SIZE  4096
+#define EASYQ_SEND_BUFFER_SIZE  512 
+#define EASYQ_HOSTNAME			"192.168.5.1"
+#define EASYQ_PORT				1085
+#define DEVICE_NAME				"dht"
+#define EASYQ_LOGIN				DEVICE_NAME
+#define FOTA_QUEUE				DEVICE_NAME":fota"
+#define FOTA_STATUS_QUEUE		DEVICE_NAME":fota:status"
+
+
+#endif
+
