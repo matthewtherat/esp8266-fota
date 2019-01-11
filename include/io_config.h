fbcff566a2db3fbfbeb3840fc52540c8f9e4ed3d
pylover <vahid.mardani@gmail.com>
2019-01-11 14:26:42 +0330

    nothing

--- include/io_config.h
+++ include/io_config.h
@@ -6,7 +6,7 @@
 #define EASYQ_RECV_BUFFER_SIZE  4096
 #define EASYQ_SEND_BUFFER_SIZE  512 
 #define EASYQ_PORT				1085
-#define EASYQ_LOGIN				"ir"
+#define EASYQ_LOGIN				"lm"
 #define DEVICE_NAME			    EASYQ_LOGIN	
 #define FOTA_QUEUE				DEVICE_NAME":fota"
 #define FOTA_STATUS_QUEUE		DEVICE_NAME":fota:status"
