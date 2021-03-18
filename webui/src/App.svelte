afa0c700bf63fec11b6129b378d176eb7053f82c
pylover <vahid.mardani@gmail.com>
2021-03-18 07:19:06 +0330

    Initial stelve project

---
+++ webui/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+import { onMount } from 'svelte';
+export let title;
+let disabled = true;
+let p = {};
+
+onMount(async () => {
+  document.title = title;
+  const res = await fetch(`/params.json`);
+  if (res.ok) {
+    p = await res.json();
+    title = document.title = `${p.zone}.${p.name}`;
+    disabled = false;
+  }
+  else {
+    throw new Error(res.text());
+  }
+});
+
+</script>
+
+<style type="text/sass">
+
+</style>
+
+<h1>{title}</h1>
+<div>
+  <p style="display: {disabled? 'block': 'none'}" >
+    Cannot load params.
+  </p>
+  <form action="/params" method="post" 
+    style="display: {disabled? 'none': 'block'}">
+
+    <h4>Settings</h4>
+    zone: <input name="zone" bind:value="{p.zone}" {disabled}/><br/>
+    name: <input name="name" bind:value="{p.name}" {disabled}/><br/>
+    AP PSK: <input name="ap_psk" bind:value="{p.apPsk}" {disabled}/><br/>
+    SSID: <input name="ssid" bind:value="{p.ssid}" {disabled}/><br/> 
+    PSK: <input name="psk" bind:value="{p.psk}" {disabled}/><br/>
+    <input type="submit" value="Reboot" {disabled} />
+  </form> 
+</div>
