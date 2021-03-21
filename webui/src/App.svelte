30f89644f93acd06d485f4978737b1b8f167cf9e
pylover <vahid.mardani@gmail.com>
2021-03-21 16:56:29 +0330

    WebUI layout

--- webui/src/App.svelte
+++ webui/src/App.svelte
@@ -2,48 +2,74 @@
 import { navOptions } from  './Nav.svelte';
 import Icons from './Icons.svelte';
 
+export let title;
 let selected = navOptions[0];
 let intSelected = 0;
 
 function changeComponent(event) {
   let i = event.srcElement.id;
+  console.log(event);
 	selected = navOptions[i];
 	intSelected = i;
 }
 </script>
 
 <style type="text/sass" global>
-  @import 'styles/global.sass'
-  nav svg 
-    height: 60px
-    cursor: pointer;
-    fill: $black;
+@import 'styles/global.sass'
+#header
+  padding: $gutter * 3
+
+nav 
+  float: $float
+  padding-left: $gutter * 3
+  a 
+    display: block
+    height: 40px
+    line-height: 40px
+    vertical-align: middle
+    cursor: pointer
+    *
+      pointer-events: none
+    h4
+      bottom: 0px
+      padding-left: $gutter
+    svg 
+      display: block
+      width: 100%
+      height: 100%
+      fill: $forecolor
     &.active
-      fill: $orange
-    use
-      pointer-events: none;
+      svg 
+        fill: $orange
 </style>
 
 <Icons />
-<div class="smc mdc">
-	<!--app navigation -->
-	<nav class="row">
-		{#each navOptions as n, i}
-      <svg id={i} class={intSelected==i ? "active md1" : "md1"} 
-           on:click={changeComponent}>
-        <use xlink:href={"#icon-" + n.icon}></use>
-      </svg>
+<div id="header" class="row">
+  <!-- Header -->
+  <div class="all10">
+    <h1>{title}</h1>
+  </div>
+</div>
 
-		{/each}
-	</nav>
-	<!-- content wrapper -->
-	<div class="row">
-		<div class="col-sm-12">
-			<div class="p-2">
-				<h1>{selected.title}</h1>
+<div class="row">
+  <!-- App navigation -->
+  <nav class="lg2">
+  	{#each navOptions as n, i}
+      <a title={n.title} id={i} 
+         class={intSelected==i ? "active row4" : "row4"} 
+         on:click={changeComponent}>
+        <svg class="lg2"><use xlink:href={"#icon-" + n.icon}></use></svg>
+        <h4 class="lg8">{n.title}</h4>
+      </a>
+  	{/each}
+  </nav>
+
+	<!-- Content wrapper -->
+	<div class="lg7">
+		<div class="row4">
+				<h2>{selected.title}</h2>
 				<!-- this is where our main content is placed -->
 				<svelte:component this={selected.component}/>
-			</div>
 		</div>
 	</div>
 </div>
--- webui/src/Nav.svelte
+++ webui/src/Nav.svelte
@@ -9,12 +9,12 @@ import Reboot from './Reboot.svelte';
 
 // these are our 'pages' to navigate through (in order for this export to work the script tag MUST include context="module")
 export const navOptions = [
-    { title: 'Home',       component: Home,       icon: 'home'       },
-		{ title: 'About',      component: About,      icon: 'question'   },
-    { title: 'Wifi',       component: Wifi,       icon: 'connection' },
-    { title: 'SourceCode', component: SourceCode, icon: 'github'     },
-    { title: 'Terminal',   component: Terminal,   icon: 'terminal'   },
-    { title: 'Reboot',     component: Reboot,     icon: 'switch'     },
+    { title: 'Home',        component: Home,       icon: 'home'       },
+		{ title: 'About',       component: About,      icon: 'question'   },
+    { title: 'Wifi',        component: Wifi,       icon: 'connection' },
+    { title: 'Source code', component: SourceCode, icon: 'github'     },
+    { title: 'Terminal',    component: Terminal,   icon: 'terminal'   },
+    { title: 'Reboot',      component: Reboot,     icon: 'switch'     },
 
 		// other navigation pages can go here
 ];
